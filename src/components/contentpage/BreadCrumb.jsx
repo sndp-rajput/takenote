eda2fce4a0065bfc2e7d952cfc665d491d2716a1
sndp-rajput <sndp.rajpuut@gmail.com>
2024-07-06 19:44:40 +0530

    Update BreadCrumb to show dynamic folder names

--- src/components/contentpage/BreadCrumb.jsx
+++ src/components/contentpage/BreadCrumb.jsx
@@ -1,9 +1,17 @@
 import { FaChevronRight } from "react-icons/fa6";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setSelectedTab } from "../../Store/Slices/DataSlices";
 
 const BreadCrumb = () => {
   const dispatch = useDispatch();
+  const { selectedTab, selectedFile, folders, tabs } = useSelector(
+    (state) => state.data
+  );
+
+  const folderName =
+    selectedTab.length < 20
+      ? tabs.filter((item) => item === selectedTab)[0]
+      : folders.filter((item) => item.id === selectedTab)[0].name;
 
   function homeHandler() {
     dispatch(setSelectedTab("folder-notes"));
@@ -11,7 +19,13 @@ const BreadCrumb = () => {
 
   return (
     <div>
-      <div className="font-cursive w-fit flex items-center gap-2 rounded px-5 py-1 border-[1px] border-black/20 dark:border-white/20 dark:text-light text-dark">
+      <div
+        className={`font-cursive w-fit grid ${
+          selectedFile
+            ? "grid-cols-[auto_auto_auto_auto_auto]"
+            : "grid-cols-[auto_auto_auto]"
+        }  max-w-[580px] items-center gap-1 rounded px-3 py-1 border-[1px] border-black/20 dark:border-white/20 dark:text-light text-dark`}
+      >
         <button
           onClick={() => {
             homeHandler();
@@ -20,9 +34,18 @@ const BreadCrumb = () => {
           Home
         </button>
         <FaChevronRight size={"10px"} />
-        <button>Categories</button>
-        <FaChevronRight size={"10px"} />
-        <button>Note</button>
+
+        <button className="whitespace-nowrap overflow-hidden text-ellipsis text-start">
+          {folderName}
+        </button>
+        {selectedFile && (
+          <>
+            <FaChevronRight size={"10px"} />
+            <button className="whitespace-nowrap overflow-hidden text-ellipsis text-start">
+              Note
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
